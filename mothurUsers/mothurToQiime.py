2773c75acd18b35e145fc66e8b606d3485cda485
neavemj <matthewjneave1@gmail.com>
2015-02-03 13:24:36 +0300

    updated explaination

--- mothurUsers/mothurToQiime.py
+++ mothurUsers/mothurToQiime.py
@@ -3,7 +3,13 @@ import sys
 # script to take the output from mothur's bin.seqs command and turn it into the output from Qiime's pick_otus.py command
 # this output can then be used with the MED-Paper-Figure-6 scripts
 
+# the bin.seqs command in mothur should be used as follows:
+# bin.seqs(list=YOURFILE.list, fasta=YOUFILE.fasta, label=0.03)
+# output: YOURFILE.0.03.fasta
 
+# this script can then be used as follows:
+# python mothurToQiime.py YOURFILE.0.03.fasta
+# the output (OTUseqID.txt) can then be used as QIIME_OTUs_FILE_PATH in 00_gen_otu_dict.py
 
 
 binSeqs_handle = sys.argv[1]
